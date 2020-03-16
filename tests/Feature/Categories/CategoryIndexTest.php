429e0ae98defc53a87e772310b585c8ab66ffae5
itohin <buhtig5102$>
2020-03-16 09:15:15 +0300

    Categories tests

--- tests/Feature/Categories/CategoryIndexTest.php
+++ tests/Feature/Categories/CategoryIndexTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Categories;
 
+use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,8 +16,39 @@ class CategoryIndexTest extends TestCase
      */
     public function test_it_returns_collections_of_categories()
     {
-        $response = $this->get('/');
+        $categories = factory(Category::class, 2)->create();
 
-        $response->assertStatus(200);
+        $response = $this->json('GET', 'api/categories');
+
+        $categories->each(function ($category) use ($response) {
+            $response->assertJsonFragment([
+                'slug' => $category->slug
+            ]);
+        });
+    }
+
+    public function test_it_returns_ordered_categories()
+    {
+        $category = factory(Category::class)->create(['order' => 2]);
+        $secondCategory = factory(Category::class)->create(['order' => 1]);
+
+        $this->json('GET', 'api/categories')
+            ->assertSeeInOrder([
+                $secondCategory->slug, $category->slug
+            ]);
+
+    }
+
+    public function test_it_returns_only_parent_categories()
+    {
+        $category = factory(Category::class)->create();
+
+        $category->children()->create(
+            factory(Category::class)->raw()
+        );
+
+        $this->json('GET', 'api/categories')
+            ->assertJsonCount(1, 'data');
+
     }
 }
