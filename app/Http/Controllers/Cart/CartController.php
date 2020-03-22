a25760f81df2f01dffbb1c02447eafdd78f25d95
itohin <buhtig5102$>
2020-03-22 16:13:55 +0300

    Cart index

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -6,6 +6,7 @@ use App\Cart\Cart;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Cart\CartStoreRequest;
 use App\Http\Requests\Cart\CartUpdateRequest;
+use App\Http\Resources\Cart\CartResource;
 use App\Models\ProductVariation;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,12 @@ class CartController extends Controller
     public function __construct()
     {
         $this->middleware(['auth:api']);
+    }
+
+    public function index(Request $request)
+    {
+        $request->user()->load(['cart.product', 'cart.product.variations.stock', 'cart.stock']);
+        return new CartResource($request->user());
     }
 
     public function store(CartStoreRequest $request, Cart $cart)
