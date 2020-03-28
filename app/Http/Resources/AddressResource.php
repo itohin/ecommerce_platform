21aa8e4ffb1be02e254114cbfe07ddb6f94b8e0d
itohin <buhtig5102$>
2020-03-28 21:49:24 +0300

    Countries api

--- app/Http/Resources/AddressResource.php
+++ app/Http/Resources/AddressResource.php
@@ -17,8 +17,10 @@ class AddressResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'city' => $this->city,
             'address_1' => $this->address_1,
             'postal_code' => $this->postal_code,
+            'default' => $this->default,
             'country' => new CountryResource($this->country)
         ];
     }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::resource('categories', 'Categories\CategoryController');
 Route::resource('products', 'Products\ProductController');
 Route::resource('addresses', 'Addresses\AddressController');
+Route::resource('countries', 'Countries\CountryController');
+
 Route::resource('cart', 'Cart\CartController', [
     'parameters' => [
         'cart' => 'productVariation'
