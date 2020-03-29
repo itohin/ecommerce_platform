f4e0e9903747236f5c6058e6095be67831434ea1
itohin <buhtig5102$>
2020-03-29 18:18:42 +0300

    Validate order store request

---
+++ app/Rules/ValidShippingMethod.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\Address;
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidShippingMethod implements Rule
+{
+    protected $addressId;
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($addressId)
+    {
+        $this->addressId = $addressId;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if (!$this->getAddress()) {
+            return false;
+        }
+
+        return $this->address->country->shippingMethods->contains('id', $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Invalid shipping method.';
+    }
+
+    protected function getAddress()
+    {
+        return Address::find($this->addressId);
+    }
+}
---
+++ tests/Feature/Orders/OrderStoreTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature\Orders;
+
+use App\Models\Address;
+use App\Models\Country;
+use App\Models\ShippingMethod;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class OrderStoreTest extends TestCase
+{
+    public function test_it_fails_if_not_authenticated()
+    {
+        $this->json('POST', 'api/orders')
+            ->assertStatus(401);
+    }
+
+    public function test_it_requires_an_address()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders')
+            ->assertJsonValidationErrors(['address_id']);
+    }
+
+    public function test_it_requires_an_address_that_exists()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders', [
+            'address_id' => 1
+        ])
+            ->assertJsonValidationErrors(['address_id']);
+    }
+
+    public function test_it_requires_an_address_that_belongs_to_user()
+    {
+        $user = factory(User::class)->create();
+
+        $address = factory(Address::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders', [
+            'address_id' => $address->id,
+            'user_id' => $user->id
+        ])
+            ->assertJsonValidationErrors(['address_id']);
+    }
+
+    public function test_it_requires_a_shipping_method()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders')
+            ->assertJsonValidationErrors(['shipping_method_id']);
+    }
+
+    public function test_it_requires_a_shipping_method_that_exists()
+    {
+        $user = factory(User::class)->create();
+
+        $this->jsonAs($user, 'POST', 'api/orders', [
+            'shipping_method_id' => 1
+        ])
+            ->assertJsonValidationErrors(['shipping_method_id']);
+    }
+
+    public function test_it_requires_a_shipping_method_valid_for_a_given_address()
+    {
+        $user = factory(User::class)->create();
+
+        $address = factory(Address::class)->create([
+            'user_id' => $user->id,
+            'country_id' => factory(Country::class)
+        ]);
+
+        $this->jsonAs($user, 'POST', 'api/orders', [
+            'shipping_method_id' => factory(ShippingMethod::class),
+            'address_id' => $address->id
+        ])
+            ->assertJsonValidationErrors(['shipping_method_id']);
+    }
+}
